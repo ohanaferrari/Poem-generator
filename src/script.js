85c037e3fff6b3a57d8db922441716c01244c3f4
ohanaferrari <ohanaferrari@gmail.com>
2025-06-24 20:33:20 -0300

    added first js

---
+++ src/script.js
@@ -0,0 +1,13 @@
+function generatePoem(event) {
+    event.preventDefault();
+
+new Typewriter(".poem", {
+    strings: "La tombe dit à la rose :<br>« Tu es belle, mais tu es éphémère. »<br>La rose répond :<br>« La beauté est un cadeau, même s'il est fugace. »",
+    autostart: true,
+    loop: false,
+    delay: 50, 
+});
+}
+
+let poemForm = document.querySelector(".poem-form");
+poemForm.addEventListener("submit", generatePoem);
