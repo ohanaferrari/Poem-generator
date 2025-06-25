a8bfe5a5c234cc1324bea28f998992222ad42e33
ohanaferrari <ohanaferrari@gmail.com>
2025-06-24 21:41:02 -0300

    final touches

--- src/script.js
+++ src/script.js
@@ -14,9 +14,12 @@ function generatePoem(event) {
 
   let input = document.querySelector(".input-text");
   let apiKey = "at320d4o2fcf70104aac4f08799bd0b0";
-  let prompt = `Generate a funny very short sarcastic poem about ${input.value}, please. Make sure to add <br /> at the end of each line, except the last.`;
+  let prompt = `Generate a funny very short sarcastic poem about ${input.value}, please. Make sure to add <br /> at the end of each line - EXCEPT the last one.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&key=${apiKey}`;
 
+  let poemElement = document.querySelector(".poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⌛ Generating your veeery serious poem... Wait a sec!</div>`;
   axios.get(apiUrl).then(displayPoem);
 }
 
