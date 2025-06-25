9891941ad32c001eb413538963d6d7e711d90741
ohanaferrari <ohanaferrari@gmail.com>
2025-06-24 21:20:33 -0300

    added ai

--- src/script.js
+++ src/script.js
@@ -1,13 +1,25 @@
-function generatePoem(event) {
-    event.preventDefault();
 
-new Typewriter(".poem", {
-    strings: "La tombe dit à la rose :<br>« Tu es belle, mais tu es éphémère. »<br>La rose répond :<br>« La beauté est un cadeau, même s'il est fugace. »",
+
+function displayPoem(response) {
+
+  new Typewriter(".poem", {
+    strings: response.data.answer,
     autoStart: true,
-    loop: false,
-    delay: 1, 
-});
+    delay: 10,
+  });
 }
 
-let poemForm = document.querySelector(".poem-form");
-poemForm.addEventListener("submit", generatePoem);
+function generatePoem(event) {
+  event.preventDefault();
+
+  let input = document.querySelector(".input-text");
+  let apiKey = "at320d4o2fcf70104aac4f08799bd0b0";
+  let prompt = `Generate a funny very short sarcastic poem about ${input.value}, please. Make sure to add <br /> at the end of each line, except the last.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
+
+let button = document.querySelector("#generateButton");
+button.addEventListener("click", generatePoem);
